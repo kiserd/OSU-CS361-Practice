2882ae5d7f0853e324771a921d872ebc3b31a896
kiserd <59572911+kiserd@users.noreply.github.com>
2020-10-30 18:04:50 -0700

    couple updates

--- HW6.js
+++ HW6.js
@@ -1,36 +1,19 @@
 var express = require('express');
-var mysql = require('./dbcon.js');
 var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
 
 var app = express();
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 6033);
+app.set('port', process.eng.PORT || 5000);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-const selectAllQuery = 'SELECT * FROM workout';
-const insertQuery = "INSERT INTO workout (`name`, `reps`, `weight`, `date`, `unit`) VALUES (?, ?, ?, ?, ?)";
-const deleteQuery = "DELETE FROM workout WHERE id=?";
-const updateQuery = "UPDATE workout SET name=?, reps=?, weight=?, date=?, unit=? WHERE id=?";
-const dropTableQuery = "DROP TABLE IF EXISTS workout";
-const makeTableQuery = `CREATE TABLE workout(
-                        id INT PRIMARY KEY AUTO_INCREMENT,
-                        name VARCHAR(255) NOT NULL,
-                        reps INT,
-                        weight INT,
-                        date DATE,
-                        unit BOOLEAN);`;
-
-
 app.get('/', (req , res, next) => {
-  var context = {};
-  res.render('home', context);
+  res.render('home');
 });
-
 
 app.use(function(req,res){
   res.status(404);
